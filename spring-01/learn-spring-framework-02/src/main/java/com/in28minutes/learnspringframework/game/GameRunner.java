189d56bb2dc415f78eab257e0aaa7c5e01551391
KARAN BABU B <karanbabu2110@gmail.com>
2024-07-28 19:57:15 +0530

    Removing Beans and Autowiring using Component

--- spring-01/learn-spring-framework-02/src/main/java/com/in28minutes/learnspringframework/game/GameRunner.java
+++ spring-01/learn-spring-framework-02/src/main/java/com/in28minutes/learnspringframework/game/GameRunner.java
@@ -1,6 +1,8 @@
 package com.in28minutes.learnspringframework.game;
 
-//PacmanGame
+import org.springframework.stereotype.Component;
+
+@Component
 public class GameRunner {
 	
 	private GamingConsole game;
